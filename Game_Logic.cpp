0de6cee02d26a5e0e4edb922d2af44a61c5a423d
Tianhao Han <kevin.hk.or2010@gmail.com>
2020-01-11 19:30:15 -0500

    FirstUpload

--- Game_Logic.cpp
+++ Game_Logic.cpp
@@ -1,3 +1,25 @@
 #include <vector>
 #include <iostream>
+#include "Main_Piece.h"
 
+using namespace std; 
+
+void Update_Board(vector<vector<char>> Board, class Piece)
+{
+
+}
+
+void Update_Row_Fullness(vector<int*> Row_Fullness, class Piece)
+{
+
+}
+
+void Shift_Down_Vector(vector<vector<char>> Board, int row_number)
+{
+
+}
+
+void Update_Score(vector<vector<char>>* Board, vector<int>* Row_Fullness, int* score)
+{
+
+}
